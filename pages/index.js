877f168735f754a883044905261a7e103dabdc9c
Ethan Skinner <ethan.skinner@equipmentshare.com>
2022-10-13 08:51:26 -0500

    Remove unused stuff

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import { Spoiler } from '../components/Spoiler';
-import { LinkList } from '../components/LinkList';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Image src={'https://i.imgur.com/WUk3xLc.jpeg'} height="500" width="500" />
+        <Image src={'https://i.imgur.com/WUk3xLc.jpeg'} alt="Moonfall movie poster" height="500" width="500" />
         <Spoiler />
       </main>
     </div>
